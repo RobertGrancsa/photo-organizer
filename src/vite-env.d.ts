9cc2289093896d56de221af902082da2d0789f5c
Robert Grancsa <robiku1975@gmail.com>
2025-02-24 12:57:21 +0200

    Initial commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({ plugins: [tailwindcss()] });
